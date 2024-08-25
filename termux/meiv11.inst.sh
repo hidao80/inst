898b40cb213c56d2a1299bcb92971c2fd51f64f0
hidao <8155294+hidao80@users.noreply.github.com>
2024-08-25 09:06:07 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -52,10 +52,11 @@ NODE_ENV=production pnpm build
 # Initialize Database
 export PG_USERNAME=example-misskey-user
 export PG_USERPASS=example-misskey-pass
-createdb misskey
-createuser $PG_USERNAME
-psql -c "ALTER USER \"$PG_USERNAME\" WITH PASSWORD '$PG_USERPASS';" misskey
+expory PG_DB_NAME=misskey
+createdb $PG_DB_NAME
+createuser -s $PG_USERNAME
+psql -c "ALTER USER \"$PG_USERNAME\" WITH PASSWORD '$PG_USERPASS';" $PG_DB_NAME
 pnpm migrate
 
 # Start Misskey-v11
-NODE_ENV=production pnpm start 2&> /dev/null
+#NODE_ENV=production pnpm start 2&> /dev/null
