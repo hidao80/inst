a57b43fdb048caec29b79c396acad4785cf3cbca
hidao <8155294+hidao80@users.noreply.github.com>
2024-08-25 15:14:31 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -49,8 +49,6 @@ cp .config/example.yml $NEW_CONF_FILE
 export FIX=src/daemons/server-stats.ts
 sed -i "9s/^/#/" $NEW_CONF_FILE
 sed -i "10s/^/url: http:\/\/$LAN_IP/" $NEW_CONF_FILE
-sed -i "18s/^/#/" $NEW_CONF_FILE
-sed -i "19s/^/port: 80/" $NEW_CONF_FILE
 sed -i "s/available: fsStats\[0\]\.available,/available: fsStats[0]?.available,/" $FIX
 sed -i "s/free: fsStats\[0\]\.available,/free: fsStats[0]?.available,/" $FIX
 sed -i "s/total: fsStats\[0\]\.size,//" $FIX
