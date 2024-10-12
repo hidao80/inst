8b5742fb1286a07d7632d73f892d952e986d5bfc
hidao <8155294+hidao80@users.noreply.github.com>
2024-10-12 10:04:11 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -50,11 +50,11 @@ cd misskey-v11
 cp .config/example.yml $NEW_CONF_FILE
 #export TARGET=src/daemons/server-stats.ts
 #sed -i "9s/^/#/" $NEW_CONF_FILE
-sed -i "9s/^.+\$/url: http:\/\/$LAN_IP$PORT/" $NEW_CONF_FILE
+sed -i "9s/.+/url: http:\/\/$LAN_IP$PORT/" $NEW_CONF_FILE
 #sed -i "13s/^/host: 0.0.0.0/" $NEW_CONF_FILE
-#sed -i "s/available: fsStats\[0\]\.available,/available: fsStats[0]?.available,/" $TARGET
-#sed -i "s/free: fsStats\[0\]\.available,/free: fsStats[0]?.available,/" $TARGET
-#sed -i "s/total: fsStats\[0\]\.size,//" $TARGET
+sed -i "s/available: fsStats\[0\]\.available,/available: fsStats[0]?.available,/" $TARGET
+sed -i "s/free: fsStats\[0\]\.available,/free: fsStats[0]?.available,/" $TARGET
+sed -i "s/total: fsStats\[0\]\.size,/total: fsStats[0]?.size,/" $TARGET
 NODE_ENV=production pnpm i
 NODE_ENV=production pnpm build
 
