0e0bd635db336ccea0c15ec55cc5f73b5e7c34ac
hidao <8155294+hidao80@users.noreply.github.com>
2024-10-13 14:55:14 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -8,7 +8,7 @@ pkg update
 yes | pkg upgrade -y
 
 # Install required packages
-yes | pkg i -y nodejs redis postgresql git ffmpeg build-essential python libvips binutils vim xorgproto
+yes | pkg i -y nodejs redis postgresql git ffmpeg build-essential python libvips binutils vim
 
 # Keep communication active even during sleep mode
 termux-wake-lock
@@ -29,40 +29,44 @@ export CFLAGS=-I\$PREFIX/include
 export LDFLAGS=-L\$PREFIX/lib
 " >> $HOME/.bashrc
 
-# Setup and Start Databases
-initdb -D $PREFIX/var/lib/postgresql
-pg_ctl -D $PREFIX/var/lib/postgresql start
-#redis-server &
-redis-server $PREFIX/etc/redis.conf --daemonize yes
 
 # Get Mei-v11 repository
 git clone --depth 1 https://github.com/mei23/misskey-v11.git
 
 # Install a node that matches the environment
 # The following packages cannot be built in Termux because they require glibc.
-npm i pnpm node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil --build-from-source
-pnpm rebuild
+#npm i pnpm node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil --build-from-source
+npm i -g pnpm
+#pnpm add node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil
+#pnpm rebuild
 
-yes | pkg upgrade -y
+#yes | pkg upgrade -y
 
 # Build Mei-v11
 cd misskey-v11
 cp .config/example.yml $NEW_CONF_FILE
-#export TARGET=src/daemons/server-stats.ts
+export TARGET=src/daemons/server-stats.ts
 #sed -i "9s/^/#/" $NEW_CONF_FILE
-sed -i "9s/.+/url: http:\/\/$LAN_IP$PORT/" $NEW_CONF_FILE
+sed -i "s/^url:.+/url: http:\/\/$LAN_IP$PORT/" $NEW_CONF_FILE
+sed -i "s/example-misskey-user/misskey/" $NEW_CONF_FILE
+sed -i "s/example-misskey-password/misskey/" $NEW_CONF_FILE
 #sed -i "13s/^/host: 0.0.0.0/" $NEW_CONF_FILE
 sed -i "s/available: fsStats\[0\]\.available,/available: fsStats[0]?.available,/" $TARGET
 sed -i "s/free: fsStats\[0\]\.available,/free: fsStats[0]?.available,/" $TARGET
 sed -i "s/total: fsStats\[0\]\.size,/total: fsStats[0]?.size,/" $TARGET
 NODE_ENV=production pnpm i
+
+# For arm64
+pnpm rebuild
+
 NODE_ENV=production pnpm build
 
-# Initialize Database
-export PG_USERNAME=example-misskey-user
-export PG_DB_NAME=misskey
-createuser -s $PG_USERNAME
-createdb -O $PG_USERNAME $PG_DB_NAME
+# Setup and Start Databases
+initdb -D $PREFIX/var/lib/postgresql
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser -s misskey
+createdb -O misskey misskey
+redis-server $PREFIX/etc/redis.conf --daemonize yes
 pnpm migrate
 
 # Start Misskey-v11
