bdf193cd1ebc1e7800eace2230249a34d2254b89
hidao <8155294+hidao80@users.noreply.github.com>
2024-10-14 10:14:53 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -8,9 +8,7 @@ pkg update
 yes | pkg upgrade -y
 
 # Install required packages
-yes | pkg i -y nodejs redis postgresql git ffmpeg build-essential python libvips binutils vim
-npm i node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil --build-from-source
-pnpm rebuild
+yes | pkg i -y nodejs redis postgresql git ffmpeg build-essential python libvips binutils vim xorgproto
 
 # Keep communication active even during sleep mode
 termux-wake-lock
@@ -24,7 +22,6 @@ export MISSKEY_DIR=$HOME/misskey
 export NEW_CONF_FILE=$MISSKEY_DIR/.config/default.yml
 export LAN_IP=$(ifconfig | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1| sort -hr | head -n 1)
 export PORT=":3000"
-export NODE_ENV=production
 echo "
 export GYP_DEFINES=\"android_ndk_path=''\"
 export PATH=¢$PATH:$HOME/node_modules/.bin
@@ -43,20 +40,19 @@ redis-server $PREFIX/etc/redis.conf --daemonize yes
 git clone --depth 1 https://github.com/mei23/misskey-v11.git $MISSKEY_DIR
 
 # Build Mei-v11
-cd $MISSKEY_DIR
-
-# For arm64
-
-
 cp $MISSKEY_DIR/.config/example.yml $NEW_CONF_FILE
 #sed -i "9s/^/#/" $NEW_CONF_FILE
 sed -i "s/url: http.*/url: http:\/\/$LAN_IP$PORT\//" $NEW_CONF_FILE
 sed -i "s/example-misskey-(user|pass)/misskey/" $NEW_CONF_FILE
-sed -i "s/fsStats\[0\]\./fsStats[0]?./" src/daemons/server-stats.ts
+sed -i "s/fsStats\[0\]\./fsStats[0]?./" $MISSKEY/src/daemons/server-stats.ts
 
 # Install a node that matches the environment
-pnpm i
-pnpm build
+npm i pnpm node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil --build-from-source
+pnpm rebuild
+
+cd $MISSKEY_DIR
+NODE_ENV=production pnpm i
+NODE_ENV=production pnpm build
 pnpm migrate
 
 # Start Misskey-v11
