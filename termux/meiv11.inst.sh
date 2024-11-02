c709b1307717b48f1f0eec3bee1fb099ad0f36b9
hidao <8155294+hidao80@users.noreply.github.com>
2024-11-02 15:36:52 +0900

    Update meiv11.inst.sh

--- termux/meiv11.inst.sh
+++ termux/meiv11.inst.sh
@@ -9,7 +9,8 @@ yes | pkg upgrade -y
 
 # Install required packages
 yes | pkg i -y nodejs redis postgresql git ffmpeg build-essential python libvips binutils vim xorgproto
-npm i pnpm node-gyp
+npm i pnpm 
+npm i node-gyp
 
 # Keep communication active even during sleep mode
 termux-wake-lock
@@ -21,6 +22,7 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export MISSKEY_DIR=$HOME/misskey
 export NEW_CONF_FILE=$MISSKEY_DIR/.config/default.yml
+export npm_config_build_from_source=true 
 export LAN_IP=$(ifconfig | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v 127.0.0.1| sort -hr | head -n 1)
 export PORT=":3000"
 echo "
@@ -46,15 +48,11 @@ sed -i "s/url: http.*/url: http:\/\/$LAN_IP$PORT\//" $NEW_CONF_FILE
 sed -i "s/example-misskey-user/misskey/" $NEW_CONF_FILE
 sed -i "s/example-misskey-pass/misskey/" $NEW_CONF_FILE
 sed -i "s/fsStats\[0\]\./fsStats[0]?./" $MISSKEY_DIR/src/daemons/server-stats.ts
-#sed -i "s/_os.cpus\(\).length/1/" $MISSKEY_DIR/built/boot/master.js
 
 # Install a node that matches the environment
-#npm i pnpm node-gyp core-js sharp msgpackr-extract utf-8-validate bufferutil --build-from-source
-#pnpm rebuild
-
 cd $MISSKEY_DIR
-NODE_ENV=production npm_config_build_from_source=true pnpm i
-NODE_ENV=production pnpm build
+pnpm -P i
+pnpm -P build
 pnpm migrate
 
 # Termux cannot read OS information.
